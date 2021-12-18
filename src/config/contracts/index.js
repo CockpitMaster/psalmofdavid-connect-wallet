cce6a7899f00a19eed137728ed01dcf7f534cd7d
flashburst <82953782+flashburst@users.noreply.github.com>
2021-12-18 18:31:09 +0530

    Added Smart Contract Source File

--- src/config/contracts/index.js
+++ src/config/contracts/index.js
@@ -3,3 +3,9 @@ export const SimpleRW = {
   97: "0xb209C02537de9A1Bf0c88B101B43540FDb8b70cF",
   80001: "0xb209C02537de9A1Bf0c88B101B43540FDb8b70cF",
 };
+
+const contracts = {
+  SimpleRW,
+};
+
+export default contracts;
